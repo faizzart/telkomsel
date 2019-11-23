0c27ad3979975dc69c88933ddcc31e7b10adb69b
faizzart <58066546+faizzart@users.noreply.github.com>
2019-11-23 13:40:35 +0700

    Update tsel.route.js

--- src/api/tsel.route.js
+++ src/api/tsel.route.js
@@ -7,5 +7,5 @@ const router = new Router();
 router.route('/magic-link').get(tselController.getMagicLink);
 router.route('/app/login').get(tselController.loginCode);
 router.route('/api/offers/filtered-offers/v3').get(tselController.apiOffersV3);
-router.route('/api/buy').post(tselController.buy);
+router.route('/api/buy').get(tselController.buy);
 module.exports = router;
